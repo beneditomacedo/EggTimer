c8515d56f22d5380a9158f3942f345efd60c856a
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-05-18 17:55:37 -0300

    fix timer to decrease instead of increase

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var timer = Timer()
+    var counter = 210
+    
+    @IBOutlet weak var counterTimer: UILabel!
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func startTimer(_ sender: Any) {
 
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                                     selector: #selector(ViewController.processTimer),
+                                     userInfo: nil, repeats: true)
+    }
+    
+    @objc func processTimer() {
+        counterTimer.text = String(counter)
+        
+        if counter <= 0 {
+            timer.invalidate()
+        } else {
+            counter -= 1
+        }
+    }
+    
+    @IBAction func minusTen(_ sender: Any) {
+        if counter > 10 {
+            counter -= 10
+        } else {
+            counter = 0
+            timer.invalidate()
+        }
+    }
+    
+    @IBAction func plusTen(_ sender: Any) {
+        counter += 10
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        counter = 210
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                                     selector: #selector(ViewController.processTimer),
+                                     userInfo: nil, repeats: true)
     }
-
-
 }
-
